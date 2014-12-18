7d595d7e05bce02d1a40b6a7c999a37b54a05c5c
JohnnyLe <lxanh@tma.com.vn>
2014-12-18 14:57:56 +0700

    Sample push notification server GCM for android

---
+++ GCMTestServer/SendNotification.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SendNotification.aspx.cs" Inherits="GCMTestServer.SendNotification" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label Text="Registration ID" runat="server" AssociatedControlID="tbRegistrationID" />
+        <asp:TextBox ID="tbRegistrationID" runat="server" />
+
+        <asp:Button Text="Submit" runat="server" />
+    </div>
+    </form>
+
+    <div id="response">
+        <p><asp:Literal ID="litResult" runat="server" /></p>
+    </div>
+</body>
+</html>
---
+++ GCMTestServer/SendNotification.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.IO;
+
+namespace GCMTestServer
+{
+    public partial class SendNotification : System.Web.UI.Page
+    {
+        // Get start
+        // http://developer.android.com/google/gcm/gs.html
+        //
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string BrowserAPIKey = "YOUR_BROWSER_API_KEY";
+                
+                string message = "some test message";
+                string tickerText = "example test GCM";
+                string contentTitle = "content title GCM";
+                string postData = "{ \"registration_ids\": [ \"" + tbRegistrationID.Text + "\" ], \"data\": {\"tickerText\":\"" + tickerText + "\", \"contentTitle\":\"" + contentTitle + "\", \"message\": \"" + message + "\"}}";
+                
+                string response = SendGCMNotification( BrowserAPIKey, postData );
+
+                litResult.Text = response;
+            }
+        }
+
+
+        /// <summary>
+        /// Send a Google Cloud Message. Uses the GCM service and your provided api key.
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="postData"></param>
+        /// <param name="postDataContentType"></param>
+        /// <returns>The response string from the google servers</returns>
+        private string SendGCMNotification(string apiKey, string postData, string postDataContentType = "application/json")
+        {
+            // from here:
+            // http://stackoverflow.com/questions/11431261/unauthorized-when-calling-google-gcm
+            //
+            // original:
+            // http://www.codeproject.com/Articles/339162/Android-push-notification-implementation-using-ASP
+
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(ValidateServerCertificate);
+
+            //
+            //  MESSAGE CONTENT
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            //
+            //  CREATE REQUEST
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("https://android.googleapis.com/gcm/send");
+            Request.Method = "POST";
+            Request.KeepAlive = false;
+            Request.ContentType = postDataContentType;
+            Request.Headers.Add(string.Format("Authorization: key={0}", apiKey));
+            Request.ContentLength = byteArray.Length;
+            
+            Stream dataStream = Request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+                        
+            //
+            //  SEND MESSAGE
+            try
+            {
+                WebResponse Response = Request.GetResponse();
+                HttpStatusCode ResponseCode = ((HttpWebResponse)Response).StatusCode;
+                if (ResponseCode.Equals(HttpStatusCode.Unauthorized) || ResponseCode.Equals(HttpStatusCode.Forbidden))
+                {
+                    var text = "Unauthorized - need new token";
+
+                }
+                else if (!ResponseCode.Equals(HttpStatusCode.OK))
+                {
+                    var text = "Response from web service isn't OK";
+                }
+
+                StreamReader Reader = new StreamReader(Response.GetResponseStream());
+                string responseLine = Reader.ReadToEnd();
+                Reader.Close();
+
+                return responseLine;
+            }
+            catch (Exception e)
+            {
+
+            }
+            return "error";
+        }
+
+
+        public static bool ValidateServerCertificate(
+                                                  object sender,
+                                                  X509Certificate certificate,
+                                                  X509Chain chain,
+                                                  SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
